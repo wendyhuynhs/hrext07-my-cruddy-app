1c007deaacff2e67c9dcb77098204eedb06ca3e6
JohnMichelin <jmichelinphone@gmail.com>
2019-02-08 12:32:02 -0800

    feat(CRUD): complete basic crud framework

--- app/app.js
+++ app/app.js
@@ -7,16 +7,25 @@ interact with localstorage
 
 $(document).ready(function(){
   // this is where we jquery
-  var keyData = 'ourKey'; // going to need to make this dynamic?
+  //var keyData = 'ourKey'; // going to need to make this dynamic?
 
 
   $('.btn-add').on('click', function(e){
     console.log(e);
+    var keyData = $('.input-key').val();
     var valueData = $('.input-value').val();
     // write to db
     localStorage.setItem(keyData, valueData);
     // read from db
-    $('.container-data').text(localStorage.getItem(keyData));
+    var displayText = keyData + ' | ' + localStorage.getItem(keyData);
+    // this only displays the last one? might want to switch to html
+    // and append a div
+    // <div class="display-data-item" data-keyValue="keyData">valueData</div>
+    // if you use backticks ` you can use ${templateLiterals}
+    // TODO make this vars make sense across the app
+    $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
+    $('.input-key').val('');
+    $('.input-value').val('');
   });
 
 
@@ -24,7 +33,9 @@ $(document).ready(function(){
     // need to expand when  more than 1 item is added
 
   // delete item
-  $('.btn-delete').click(function(){
+  $('.container-data').on('click', '.display-data-item', function(e){
+    console.log(e.currentTarget.dataset.keyvalue);
+    var keyData = e.currentTarget.dataset.keyvalue;
     localStorage.removeItem(keyData);
     $('.container-data').text('');
   });
