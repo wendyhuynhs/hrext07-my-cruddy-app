c135df2236d4306eb7df43575a85a019540092b5
JohnMichelin <jmichelinphone@gmail.com>
2019-02-08 11:39:29 -0800

    feat(localstorage): wire front end with back end

--- app/app.js
+++ app/app.js
@@ -7,16 +7,29 @@ interact with localstorage
 
 $(document).ready(function(){
   // this is where we jquery
+  var keyData = 'ourKey'; // going to need to make this dynamic?
+
+
   $('.btn-add').on('click', function(e){
     console.log(e);
+    var valueData = $('.input-value').val();
+    // write to db
+    localStorage.setItem(keyData, valueData);
+    // read from db
+    $('.container-data').text(localStorage.getItem(keyData));
   });
-  // write to db
 
-  // read from db
 
   // update db
+    // need to expand when  more than 1 item is added
 
   // delete item
-
+  $('.btn-delete').click(function(){
+    localStorage.removeItem(keyData);
+  });
   // delete all?
+  $('.btn-clear').click(function(){
+    localStorage.clear();
+  });
+
 });
